c1483755f9a06ee82dd415eada2117a328c6eff2
Leandro Marcelino <leandro.souza.marcelino@gmail.com>
2021-12-04 23:19:39 -0300

    Criando API para pegar distancia entre duas cidades -  Criando a classe DistanceResource e DistanceService - Criando métodos distanceByPoints e  distanceByCube na interface CityRepository.

--- src/main/java/com/dio/leandrosouza/citiesapi/cities/CityRepository.java
+++ src/main/java/com/dio/leandrosouza/citiesapi/cities/CityRepository.java
@@ -3,4 +3,7 @@ package com.dio.leandrosouza.citiesapi.cities;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<City, Long> {
+    Double distanceByPoints(Long city1, Long city2);
+
+    Double distanceByCube(double x, double y, double x1, double y1);
 }
